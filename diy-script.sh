359f5b13e44c41774db986009c9be6c985dc59eb
sean <siryle1213@gmail.com>
2024-12-12 08:04:27 +0000

    modified:   Dockerfile_1 	modified:   Dockerfile_2 	modified:   Dockerfile_3 	modified:   config_1 	new file:   diy-script.sh 	deleted:    imm_patch/1.5g.patch 	deleted:    imm_patch/add_pwm_fan.sh.patch 	deleted:    imm_patch/resizing_filesystem_at_first_boot.patch 	deleted:    imm_patch/swap_wan_and_lan.patch 	deleted:    imm_patch/wan_ACCEPT.patch 	deleted:    resize_rootfs.sh

---
+++ diy-script.sh
@@ -0,0 +1,14 @@
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 添加额外插件
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-netspeedtest homebox
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+
